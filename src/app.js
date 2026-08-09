18edeaace46da9dae166918a4583d6174e461772
Dhruv Panchal <dhruvpanchal1345@gmail.com>
2026-08-09 16:27:23 +0530

    add frontend url to cors

--- src/app.js
+++ src/app.js
@@ -19,7 +19,11 @@ const app = express();
 const server = createServer(app);
 
 app.set("port", process.env.PORT || 8000);
-app.use(cors());
+app.use(cors({
+    origin: "https://live-desk-frontend.vercel.app",
+    methods: ["GET", "POST"],
+    credentials: true
+}));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 app.use("/api/v1/users", userRoutes);
