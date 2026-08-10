d9fad0553a4510297298287c38880bd8c6cefadd
Dhruv Panchal <dhruvpanchal1345@gmail.com>
2026-08-10 14:36:00 +0530

    remove Unnecessary comments

--- src/controllers/socketManager.js
+++ src/controllers/socketManager.js
@@ -15,25 +15,25 @@ export const connectToSocket = async (server) => {
 
   io.on('connection', async (socket) => {
 
-    // ✅ WebRTC - Room join
+    //WebRTC - Room join
     socket.on('join-call', (path) => {
       if (connections[path] === undefined) {
         connections[path] = [];
       }
       connections[path].push(socket.id);
 
-      // Sabko batao naya user aaya
+      
       connections[path].forEach(id => {
         io.to(id).emit('user-joined', socket.id, connections[path]);
       });
     });
 
-    // ✅ WebRTC - Signal (SDP/ICE)
+    //WebRTC - Signal (SDP/ICE)
     socket.on('signal', (toId, message) => {
       io.to(toId).emit('signal', socket.id, message);
     });
 
-    // ✅ Chat message
+    // Chat message
     socket.on('chat-message', (data, sender) => {
       connections[Object.keys(connections).find(key => 
         connections[key].includes(socket.id)
@@ -42,7 +42,7 @@ export const connectToSocket = async (server) => {
       });
     });
 
-    // ✅ Disconnect
+    //Disconnect
     socket.on('disconnect', () => {
       Object.keys(connections).forEach(path => {
         connections[path] = connections[path].filter(id => id !== socket.id);
